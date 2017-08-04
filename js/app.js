b24fce82f13b84e27384a5e4040272b139bfc863
WW930\A667661 <krzysztof.2.mazurkiewicz@atos.net>
2017-08-04 09:31:44 +0200

    some imgs

--- js/app.js
+++ js/app.js
@@ -3,16 +3,12 @@ $(document).ready(function(){
 });
 
 var PEPSI = (function(){
-	
-
 	function dev(){
-		$('body').on('click', event => { console.log(window.innerWidth);
-				console.log($('body').width())});
+
 	}
 	return{
 		init: function(){
 			dev();
-			// $('main').scrollLeft($('main').width() /2); 
 		}
 	}
 })();
