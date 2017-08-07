1aab762cbc73d0fae617135e131ec540750e1714
WW930\A667661 <krzysztof.2.mazurkiewicz@atos.net>
2017-08-07 12:56:59 +0200

    tilt working

--- js/app.js
+++ js/app.js
@@ -5,51 +5,43 @@ $(document).ready(function(){
 var PEPSI = (function(){
 	var	mainHeight = $('main').height(),
         mainWidth = $('main').width(),
-		loopInterval = 30,
+		loopInterval = 50,
 		loopCounter = 0,
-		interval,
-		data;
+        intervalVar,
+		data,
+		$elements = [];
 
 	var ANIMS = {
-		move: function(element, values) {
-			var $element = $('#'+element.id);
-				 eleft = $element.position().left,
-				 etop = $element.position().top;
-				 console.log(top + values[0] +'px');
-			$element.css({
-				 top: etop + values[1] +'px',
-				 left: eleft + values[0] +'px'
-			})
+		move: function(index, values) {
+			TweenMax.to($elements[index], 0, {x: values[0], y: values[1]});
 		},
-		rotate: function(element, arr) {
-			// console.log(element);
-			// console.log(arr);
+		rotate: function(index, arr) {
+			TweenMax.to($elements[index], 0, {rotation: arr[0]});
+		},
+		changeBackground: function(index, values) {
+			var path = 'url(../img/'+values[0]+'.png';
+            TweenMax.to($elements[index], 0, {background: path});
+		},
+		brightnes: function(index, values) {
+			TweenMax.to($elements[index], 0, {filter: 'brightness('+values+'%)'});
 		}
 	};
 
 	function animateElements() {
-		data.forEach(function(element){
-			if(loopCounter % element.time == 0){
-				// element.anims.forEach(function(anim){
-				// 	 anim.forEach(function(singleAnim){
-				// 		ANIMS[singleAnim.type](element, singleAnim.values);
-				//
-				// 	 });
-				// });
-				// element.anims[element.counter].forEach(function(anim){
-				// 	ANIMS[anim.type](element, anim.values);
-				// });
-				for(var i = 0; i < element.anims[element.counter].length; i++)
-                     	ANIMS[element.anims[element.counter][i].type](element, element.anims[element.counter][i].values);
-				element.counter++;
-				if(element.counter >= element.anims.length) element.counter = 0;
-			}
+		data.forEach(function(element, index){
+			if(element.time)
+				if(loopCounter % element.time == 0){
+					for(var i = 0; i < element.anims[element.counter].length; i++)
+							ANIMS[element.anims[element.counter][i].type](index, element.anims[element.counter][i].values);
+					element.counter++;
+					if(element.counter >= element.anims.length) element.counter = 0;
+				}
 		});
 		loopCounter += loopInterval;
 	};
 
 	function startMainLop(){
-		interval = setInterval(animateElements, loopInterval);
+		intervalVar = setInterval(animateElements, loopInterval);
 	};
 
 	function initGraphs() {
@@ -59,54 +51,34 @@ var PEPSI = (function(){
             newElement.css({background: path,
 				top: mainHeight / 100 * element.y - (element.height / 2),
 				left: mainWidth / 100 * element.x - (element.width / 2),
-				width: element.width+'px',
-				height: element.height+'px',
+				width: mainWidth / 100 * element.width+'px',
+				height: mainHeight / 100 * element.height+'px',
 				zIndex: element.z
 			});
 			$('main').append(newElement);
+			$elements.push(newElement);
 		});
-    }
+    };
 
-
-
-	function dev(){
-		$('body').on('click', function(){
-        });
+	function initEvents() {
+		var center = mainWidth / 2.7;
+		if(window.DeviceOrientationEvent) {
+			window.addEventListener('deviceorientation', function(event){
+				window.scrollTo(center - (event.alpha* 10), 0);
+			});
+		}
 	}
+
 	return{
 		init: function(DATA){
 			data = DATA;
-			dev();
-            $('body').scrollLeft(mainWidth / 3);
 			initGraphs();
+			initEvents();
             startMainLop();
+            console.log(mainWidth/3);
+            window.scrollTo(0,0);
+            setTimeout(function(){window.scrollTo(mainWidth/2.7, 0)}, 500);
 		}
 	}
 })();
 
-var DATA = [
-	{ time: 100,
-		x: 50,
-		y: 20,
-		z: 2,
-		counter: 0,
-		width: 100,
-		height: 100,
-		id: 'pepsi2',
-		anims: [
-				[{type: 'move', values: [40,50]}, {type: 'rotate', values: [40]}],
-				[{type: 'move', values: [-50,-50]}]
-			]
-	},
-    // { time: 30,
-    //     x: 50,
-    //     y: 50,
-    //     z: 2,
-    //     counter: 0,
-    //     width: 100,
-    //     height: 100,
-    //     id: 'pepsi1',
-    //     anims: [[['move',20,20], ['rotate',40]],
-    //         [['move',-20,-20]]]
-    // },
-]
